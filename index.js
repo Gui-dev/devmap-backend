5c368406cebf4d8841a9d599104fdfc48f8c6960
Gui-dev <adrianon10@gmail.com>
2020-01-20 14:05:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const app = require( './src/App' )
+const port = process.env.PORT || 3333
+
+app.listen( port, () => {
+    console.log( `Servidor rodando na porta ${port}` )
+} )
---
+++ src/App.js
@@ -0,0 +1,37 @@
+require( 'dotenv' ).config()
+const express = require( 'express' )
+const mongoose = require( 'mongoose' )
+const cors = require( 'cors' )
+
+const routes = require( './routes' )
+const databaseConfig = require( './config/database' )
+
+class App {
+
+  constructor() {
+    this.app = express()
+
+    this.database()
+    this.middlewares()
+    this.routes()
+  }
+
+  database() {
+    mongoose.connect( databaseConfig.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    } )
+  }
+
+  middlewares() {
+    this.app.use( express.json() )
+    this.app.use( cors() )
+  }
+
+  routes() {
+    this.app.use( routes )
+  }
+
+}
+
+module.exports = new App().app
